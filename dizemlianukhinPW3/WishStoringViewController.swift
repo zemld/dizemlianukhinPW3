2cc322127b73ff90637cb5fc7e1703e8dd98b9e7
Sunr1seColours <zemlyanukhind@yandex.ru>
2024-11-10 11:25:47 +0300

    Сделал пункт 3

--- dizemlianukhinPW3/WishStoringViewController.swift
+++ dizemlianukhinPW3/WishStoringViewController.swift
@@ -8,7 +8,42 @@
 import UIKit
 
 final class WishStoringViewController: UIViewController {
+    // MARK: - Constants
+    private enum Constants {
+        static let tableCornerRadius: CGFloat = 10
+        static let tableOffset: CGFloat = 30
+    }
+    
+    // MARK: - Variables
+    private let table: UITableView = UITableView(frame: .zero)
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         view.backgroundColor = .blue
+        configureTable()
     }
+    
+    // MARK: - Private methods
+    private func configureTable() {
+        view.addSubview(table)
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.tableCornerRadius
+        
+        table.pin(to: view, Constants.tableOffset)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
